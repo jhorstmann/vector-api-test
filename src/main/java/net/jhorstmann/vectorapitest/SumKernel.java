72bb3c28ec83d93960da2575d661d2a01ecbbbde
Jörn Horstmann <git@jhorstmann.net>
2020-10-20 22:45:25 +0200

    Blend as longs

--- src/main/java/net/jhorstmann/vectorapitest/SumKernel.java
+++ src/main/java/net/jhorstmann/vectorapitest/SumKernel.java
@@ -5,19 +5,29 @@ import jdk.incubator.vector.*;
 public class SumKernel {
     private static final VectorSpecies<Double> F64X4 = DoubleVector.SPECIES_256;
     private static final VectorSpecies<Long> I64X4 = LongVector.SPECIES_256;
+    private static final VectorSpecies<Integer> I32X8 = IntVector.SPECIES_256;
     private static final LongVector IDX_I64X4 = LongVector.fromArray(I64X4, new long[]{8, 4, 2, 1}, 0);
     private static final DoubleVector IDX_F64X4 = IDX_I64X4.reinterpretAsDoubles();
 
-    private static VectorMask<Double> maskFromBits(long mask) {
+    private static final IntVector IDX_I32X8 = IntVector.fromArray(I32X8, new int[]{128, 64, 32, 16, 8, 4, 2, 1}, 0);
+    private static final FloatVector IDX_F32X8 = IDX_I32X8.reinterpretAsFloats();
+
+    private static VectorMask<Long> maskFromBits(long mask) {
         var broadcast = LongVector.broadcast(I64X4, mask & 0x0F);
-        return IDX_I64X4.and(broadcast).reinterpretAsDoubles().eq(IDX_F64X4);
+        return IDX_I64X4.and(broadcast).eq(IDX_I64X4);
     }
 
     private static DoubleVector maskedSum(DoubleVector accumulator, double[] data, int i, long mask) {
         var vmask = maskFromBits(mask);
 
-        var v1 = DoubleVector.fromArray(F64X4, data, i, vmask);
-        return accumulator.add(v1);
+        var v1 = DoubleVector.fromArray(F64X4, data, i);
+
+        // blend as longs in order to use VectorMask<Long>, which should be slightly faster to compute
+        var blended = v1.reinterpretAsLongs()
+                .blend(Double.doubleToLongBits(0.0), vmask)
+                .reinterpretAsDoubles();
+
+        return accumulator.add(blended);
     }
 
     public static double sum(double[] data, byte[] valid) {
